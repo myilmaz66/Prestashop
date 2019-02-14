c8244db9923ec7a44a03179582686726efad882a
“myilmaz66” <mmyilmaz66@gmail.com>
2019-02-14 13:25:21 -0500

    remove src

--- src/test/java/com/prestashop/utilities/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.prestashop.utilities;
-
-public class AppConstants {
-    public static final String ADD_TO_CART_MESSAGE = "Product successfully added to your shopping cart";
-    public static final String WRON_PSWRD_MSG ="The password you entered is incorrect.";
-    public static final String FIRSTNAME_REQUIRED_MSG ="firstname is required.";
-}
